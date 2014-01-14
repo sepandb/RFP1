4ab2b2b28ea2fb0080e5a84d4c3f7e1149463401
Sep Bar <sepand.barkhodaee@gmail.com>
2014-01-14 14:02:19 -0500

    exercise 2 updated

--- exercise2.rb
+++ exercise2.rb
@@ -6,3 +6,19 @@ end
 total = tipcalculator(55,0.2)
 
 puts "You should tip #{total} on top of your bill"
+
+#Solution to question 2
+
+#Solution to question 3
+a = 45628
+b = 7839
+
+puts "The value of #{a} multipled by #{b} is #{a*b}"
+
+#Solution to question 4
+x = (true && false) || (false && true) || !(false && false)
+if x
+	puts "this is true"
+else
+	puts "this is false"
+end
