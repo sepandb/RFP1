fee8f81371ea9fe6a9e8685ab4fe6c1030d79c2f
Sep Bar <sepand.barkhodaee@gmail.com>
2014-01-14 14:11:12 -0500

    Display user's date of birth

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,9 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "Hi #{name}!"
+puts "How old are you?"
+
+age = gets.chomp.to_i
+yob1 = 2014 - age
+yob2 = yob1 -1 
+puts "You must have been born in #{yob1} or #{yob2}!"
